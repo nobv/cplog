71fd8b2fc36db9262b8b7f6cfbd09dac1813210c
nobv <36393714+nobv@users.noreply.github.com>
2023-07-26 21:40:12 +0900

    practice

---
+++ atcoder/python/beginners_selection/coins.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+
+res = 0
+for _a in range(a + 1):
+    for _b in range(b + 1):
+        for _c in range(c + 1):
+            total = 500 * _a + 100 * _b + 50 * _c
+            if total == x:
+                res += 1
+print(res)
--- atcoder/python/beginners_selection/placing_marbles.py
+++ atcoder/python/beginners_selection/placing_marbles.py
@@ -1,4 +1,4 @@
 # https://atcoder.jp/contests/abs/tasks/abc081_a
-inp = input()
-res = [i for i in str(inp) if i == "1"]
+inp = str(input())
+res = [i for i in inp if i == "1"]
 print(len(res))

ac9f1901f63c72a7d2d6b366feaa819755d59ee9
nobv <36393714+nobv@users.noreply.github.com>
2023-07-28 10:52:03 +0900

    practice

--- atcoder/python/beginners_selection/kagami_mochi.py
+++ atcoder/python/beginners_selection/kagami_mochi.py
@@ -1,3 +1,8 @@
 # https://atcoder.jp/contests/abs/tasks/abc085_b
 _, *d = map(int, open(0).read().split())
 print(len(set(d)))
+
+# a little faster than above
+# N = int(input())
+# di = [int(input()) for _ in range(N)]
+# print(len(set(di)))

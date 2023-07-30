9c07e5384a1a520b0d0d64dda03a9a1780c28c7c
nobv <36393714+nobv@users.noreply.github.com>
2023-07-31 00:06:20 +0900

    practice

--- atcoder/python/abc/312/c_invisible_hand.py
+++ atcoder/python/abc/312/c_invisible_hand.py
@@ -4,3 +4,25 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 print(sorted(A + list(map(lambda x: x + 1, B)))[M - 1])
+
+
+# other answer
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# B = list(map(int, input().split()))
+# wa = 0
+# ac = 1001001001
+# while wa + 1 < ac:
+#     wj = int((wa + ac) / 2)
+#     na = nb = 0
+#     for i in range(N):
+#         if A[i] <= wj:
+#             na += 1
+#     for i in range(M):
+#         if B[i] >= wj:
+#             nb += 1
+#     if na >= nb:
+#         ac = wj
+#     else:
+#         wa = wj
+# print(ac)

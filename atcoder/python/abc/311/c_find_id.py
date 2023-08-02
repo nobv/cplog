17185eaf4322521e820b7f7b9f42d4a1017f6d84
nobv <36393714+nobv@users.noreply.github.com>
2023-08-02 10:18:30 +0900

    practice

---
+++ atcoder/python/abc/311/c_find_id.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc311/tasks/abc311_c
+N = int(input())
+A = list(map(int, ("0 " + input()).split()))
+now = 1
+for _ in range(N):
+    now = A[now]
+B = [now]
+while B[0] != A[now]:
+    now = A[now]
+    B.append(now)
+print(len(B))
+print(*B)
